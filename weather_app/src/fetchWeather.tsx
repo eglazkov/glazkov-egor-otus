050e47663422334e45673a89847a3db09a6b8182
eglazkov <egorglazkov@mail.ru>
2020-01-22 02:42:10 +0300

    Routing в React — оптимизация приложения diff по теме занятия

--- weather_app/src/fetchWeather.tsx
+++ weather_app/src/fetchWeather.tsx
@@ -3,10 +3,10 @@ import {fetchWeatherPending, fetchWeatherSuccess, fetchWeatherError} from './wea
 function fetchWeather(cityName) {    
     return dispatch => {
         dispatch(fetchWeatherPending());
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=1ddd872d319ebc1ceaa4fcec9d9cd620&units=metric`)
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${cityName}&cnt=7&APPID=1ddd872d319ebc1ceaa4fcec9d9cd620&units=metric`)
         .then(res => res.json())
         .then(res => {            
-            if(res.cod !== 200) {
+            if(String(res.cod) !== '200') {
                 throw(res.message);
             }
             dispatch(fetchWeatherSuccess(res));
